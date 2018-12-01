ddc1ee2a1834c5722823f2bb5592db4c0c35c231
Korcan Uyanik <korcan_uyanik@sfu.ca>
2018-12-01 14:33:37 -0800

    Implemented, beta

--- id3.cpp
+++ id3.cpp
@@ -5,16 +5,6 @@
 #include <math.h>
 using namespace std;
 
-bool not_equal(string str, vector<string> str_arr){
-	int i = 0;
-	while(i < str_arr.size()){
-		if(str == str_arr[i])
-			return false;
-		++i;
-	}
-	return true;
-}
-
 int count_att(string str){
 	int i = 0;
 	int count = 0;
@@ -47,27 +37,6 @@ struct obj{
 	}
 };
 
-vector<string> find_types(vector<obj> arr_obj){ //finds type names: setosa,versicolor. Can be altered to find more than 2 types(via remember) but according to Karol we deal with 2 types max.
-						//returns null vector if array is size 0 or else returns 1 or 2 unique types.
-	if(arr_obj.size() == 0){
-		return vector<string>();
-	}
-
-	vector<string> unique;
-	unique.push_back(arr_obj[0].type);
-
-	int i = 0;
-	while(i < arr_obj.size()){
-		if(arr_obj[0].type != arr_obj[i].type){
-			unique.push_back(arr_obj[i].type);
-			break;
-		}
-		++i;
-	}
-
-	return unique;
-}
-
 void extract(vector<obj> &arr_obj, string &str, unsigned int &count_comma){
 
 	string temp_s;
@@ -89,52 +58,71 @@ void extract(vector<obj> &arr_obj, string &str, unsigned int &count_comma){
 
 }
 
+vector<string> find_types(vector<obj> arr_obj){ //finds type names: setosa,versicolor. Can be altered to find more than 2 types(via remember) but according to Karol we deal with 2 types max.
+	if(arr_obj.size() == 0){		//returns null vector if array is size 0 or else returns 1 or 2 unique types.
+		return vector<string>();
+	}
+	vector<string> unique;
+	unique.push_back(arr_obj[0].type);
+	int i = 0;
+	while(i < arr_obj.size()){
+		if(arr_obj[0].type != arr_obj[i].type){
+			unique.push_back(arr_obj[i].type);
+			break;
+		}
+		++i;
+	}
+	return unique;
+}
+
 struct node{
 	vector <obj> arr_obj;
+	vector<string> decision, unique_types;
 	node* left;
 	node* right;
-	string decision;
 
-	node (vector<obj> arr_obj, string decision){
+	node (vector<obj> arr_obj,vector<string> unique_types, vector<string> decision = {"At root node"}){
+		this->unique_types = unique_types;
 		this->arr_obj = arr_obj;
+		this->decision = decision;
 		left = nullptr;
 		right = nullptr;
-		this->decision = decision;
+		split();
+	}
+	vector<int> each_type_count(vector<obj> arr_obj){
+		int type1 = 0,type2 = 0;
+		int i = 0;
+		while(i < arr_obj.size()){
+			if(arr_obj[i].type == this->unique_types[0]){
+				++type1;
+			}
+			else if(arr_obj[i].type == this->unique_types[1])
+				++type2;
+			++i;
+		}
+		return vector<int>{type1,type2};
 	}
 
 	double entropy(vector<obj> arr_obj){
-		vector<string> unique_types = find_types(arr_obj);
-		if(unique_types.size() == 1){ //homogenous
+		vector<int> type_count = each_type_count(arr_obj);
+		if(type_count[0] == 0 || type_count[1] == 0){ //homogenous or empty
 			return 0;
 		}
 
-		if(unique_types.size() == 0){//empty
-			return 0;
-		}
-
-		int i = 0;
-		double no = 0,yes = 0;
-		while(i < arr_obj.size()){
-			if(arr_obj[i].type == unique_types[0])
-				++no;
-			else if(arr_obj[i].type == unique_types[1])
-				++yes;
-			++i;
-		}
-		double p_no = no/(no+yes);
-		double p_yes = yes/(no+yes);
+		double p_no =  double(type_count[0])/double(( type_count[0]+type_count[1]));
+		double p_yes =  double(type_count[1])/double(( type_count[0]+ type_count[1]));
 		double entropy = -(p_no*log2(p_no))-(p_yes*log2(p_yes));
-
 		return entropy;
 	}
 
-	vector<double> find_splitp(){//find split point
+	vector<double> find_split_point(vector<obj> arr_obj){//find split point
 		double gain = 0, cutoff = 0, att_cutoff = 0;
 		vector<obj> smaller,greater;
-		int i = 0, j = 0;
+		int i = 0;
+		int j = 0;
 		while(i < arr_obj[0].att.size()){
 			j = 0;
-			while(j < arr_obj.size()){
+ 			while(j < arr_obj.size()){
 				double temp_cutoff = arr_obj[j].att[i];
 				int k = 0;
 				while(k < arr_obj.size()){
@@ -146,10 +134,6 @@ struct node{
 					}
 					++k;
 				}
-				if(temp_cutoff == 1.9){
-					int i =0;
-					while(i < greater.size()){ greater[i].print(); ++i;}
-				}
 				double pe1 = entropy(arr_obj);
 				double pe2 = entropy(smaller);
 				double pe3 = entropy(greater);
@@ -157,6 +141,7 @@ struct node{
 				double size_smaller = smaller.size();
 				double size_greater = greater.size();
 				double size_all = arr_obj.size();
+
 				double temp_gain = pe1 - ((size_smaller/size_all)*pe2) - ((size_greater/size_all)*pe3);
 				if(temp_gain > gain){
 					cutoff = temp_cutoff;
@@ -170,39 +155,56 @@ struct node{
 			++i;
 		}
 		cout<<gain<<endl;
+		//cout<<"KORCAN"<<endl;
 		return vector<double>{cutoff,att_cutoff};
 	}
 
-	node* split(node* circle){
-		if(find_types(circle->arr_obj).size() < 2){
-			return nullptr;
+	void split(){
+		vector<int> type_count = each_type_count(this->arr_obj);
+		if(type_count[0] == 0 || type_count[1] == 0){ //homogenous or empty
+			return;
 		}
 
-		vector<double> splitp = circle->find_splitp(); // index 0 cutoff , index 1 attribute
-		cout<<splitp[0]<<splitp[1]<<endl;
+		vector<double> split_point = find_split_point(this->arr_obj); // index 0 cutoff , index 1 attribute
 		vector<obj> smaller,greater;
 		int i = 0;
-		while(i < circle->arr_obj.size()){
-			double cutoff = splitp[0];
-			if(circle->arr_obj[i].att[static_cast<int>(splitp[1])] <= splitp[0]){
-				smaller.push_back(circle->arr_obj[i]);
+		while(i < arr_obj.size()){
+			double cutoff = split_point[0];
+			if(arr_obj[i].att[static_cast<int>(split_point[1])] <= split_point[0]){
+				smaller.push_back(arr_obj[i]);
 			}
-			else if(circle->arr_obj[i].att[static_cast<int>(splitp[1])] > splitp[0]){
-				greater.push_back(circle->arr_obj[i]);
+			else if(arr_obj[i].att[static_cast<int>(split_point[1])] > split_point[0]){
+				greater.push_back(arr_obj[i]);
 			}
 			++i;
 		}
-		string s_ltemp = ("Attribute " + to_string(splitp[1]) + " <= " + to_string(splitp[0]) );
-		string s_rtemp = ("Attribute " + to_string(splitp[1]) + " > " + to_string(splitp[0]) );
-
-		circle->left = new node(smaller, s_ltemp);
-		circle->right = new node(greater, s_rtemp);
+		string s_ltemp = ("Attribute_" + to_string( int(split_point[1]+1)) + " <= " + to_string(split_point[0]));
+		string s_rtemp = ("Attribute_" + to_string( int(split_point[1]+1)) + " > " + to_string(split_point[0]));
+		this->decision.push_back(s_ltemp);
+		left = new node(smaller,this->unique_types, this->decision);
+		this->decision.pop_back();
+		this->decision.push_back(s_rtemp);
+		right = new node(greater,this->unique_types, this->decision);
+		this->decision.pop_back();
 //		cout<<"SIZE OF SMALLER "<<smaller.size()<<" SIZE GREATER "<<greater.size()<<endl;
-		circle->left = split(circle->left);
-		circle->right = split(circle->right);
 	}
 
+	void node_print(){
+		if(left == nullptr && right == nullptr){
+			int i = 0;
+			while(i < decision.size()){
+				cout<<decision[i]<<endl;
+				++i;
+			}
+			vector<int> type_count = each_type_count(arr_obj);
+			cout<<"Number of total items: "<<arr_obj.size()<<endl;
+			cout<<unique_types[0]<<" count is "<<type_count[0]<<" and "<<unique_types[1]<<" count is "<<type_count[1]<<endl;
+			return;
+		}
 
+		left->node_print();
+		right->node_print();
+	}
 };
 
 int main(int argc,char **argv){
@@ -226,13 +228,8 @@ int main(int argc,char **argv){
 				extract(arr_obj, line, comma_count);// very important, extracts the data from the file into objects.
 		}
 	}
-	cout<<"OBJECT LENGTH"<<arr_obj.size()<<endl;
-
-	node* root = new node(arr_obj, "root has no decision");
-
-	vector<double> split_p = root->find_splitp();
-	cout<<"CUTOFF "<< split_p[0] <<", ATTRIBUTE AT CUTOFF "<< split_p[1] <<endl;
-
-	//root->split(root);
+	cout<<"OBJECT LENGTH: "<<arr_obj.size()<<endl;
+	node* root = new node(arr_obj, find_types(arr_obj));
+	root->node_print();
 	return 0;
 }
